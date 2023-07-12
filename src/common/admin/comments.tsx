8b945360afeabbbb94b135c66f1a8273dc0ef6bd
fayzikuloff.b <fayzikuloff.b@gmail.com>
2023-07-12 16:31:58 +0500

    :sparkles: Admin entities define

---
+++ src/common/admin/comments.tsx
@@ -0,0 +1,38 @@
+import {
+  List,
+  Datagrid,
+  ReferenceField,
+  TextField,
+  Edit,
+  SimpleForm,
+  TextInput,
+  required,
+  ReferenceInput
+} from "react-admin";
+
+export const CommentList = () => {
+  return (
+    <List>
+      <Datagrid rowClick={"edit"}>
+        <TextField source={"id"} />
+        <TextField source="text" />
+        <ReferenceField source="userId" reference="users" />
+        <ReferenceField source="itemId" reference="items" />
+      </Datagrid>
+    </List>
+  )
+}
+
+
+export const CommentEdit = () => {
+  return (
+    <Edit>
+      <SimpleForm>
+        <TextInput disabled label={"id"} source={"id"} name={"id"}/>
+        <TextInput name={"text"} source={"text"} validate={required()} />
+        <ReferenceInput source="userId" reference="users" name={"users"}/>
+        <ReferenceInput source="itemId" reference="items" name={"items"}/>
+      </SimpleForm>
+    </Edit>
+  )
+}
--- src/common/admin/index.tsx
+++ src/common/admin/index.tsx
@@ -2,6 +2,7 @@ import {Admin, ListGuesser, Resource} from "react-admin";
 import {UserCreate, UserEdit} from "./users";
 import {CollectionCreate, CollectionEdit, CollectionList} from "./collections";
 import dataProvider from "./dataProvider";
+import {CommentEdit, CommentList} from "./comments";
 
 export const AdminDashboard = () => {
   return (
@@ -12,6 +13,9 @@ export const AdminDashboard = () => {
     >
       <Resource name="users" list={ListGuesser} edit={UserEdit} create={UserCreate}/>
       <Resource name={"collections"} list={CollectionList} edit={CollectionEdit} create={CollectionCreate}/>
+      <Resource name={"items"} list={ListGuesser} />
+      <Resource name={"comments"} list={CommentList} edit={CommentEdit} />
+      <Resource name={"likes"} list={ListGuesser} />
     </Admin>
   );
 }
